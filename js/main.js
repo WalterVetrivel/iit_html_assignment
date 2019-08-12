f8b968185f464ff24e2dd612348eef081c29779b
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-08-13 03:13:47 +1000

    Minor improvements to responsiveness

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ const dropdown = document.querySelector('.nav-dropdown');
 
 let isDropdownVisible = false;
 
-dropdownToggle.addEventListener('click', () => {
+const toggleDropdown = () => {
 	if (!isDropdownVisible) dropdown.classList.add('show-dropdown');
 	else dropdown.classList.remove('show-dropdown');
 	isDropdownVisible = !isDropdownVisible;
-});
+};
+
+dropdownToggle.addEventListener('click', toggleDropdown);
 
 const navToggle = document.querySelector('.nav-toggle');
 const navItems = document.querySelector('.nav-items');
 
 let isNavVisible = false;
 
-navToggle.addEventListener('click', () => {
+const toggleNav = () => {
 	if (!isNavVisible) navItems.classList.add('show-nav');
 	else navItems.classList.remove('show-nav');
 	isNavVisible = !isNavVisible;
-});
+};
+
+navToggle.addEventListener('click', toggleNav);
